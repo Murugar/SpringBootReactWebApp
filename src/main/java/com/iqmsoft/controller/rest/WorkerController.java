3f9d92502511854c95640d710bef703476e46fad
Muruga <davanon2014@gmail.com>
2021-09-21 16:46:01 +0530

    First

--- src/main/java/com/iqmsoft/controller/rest/WorkerController.java
+++ src/main/java/com/iqmsoft/controller/rest/WorkerController.java
@@ -15,10 +15,10 @@ public class WorkerController {
 
     @GetMapping("/workers")
     public ResponseEntity<?> employee() {
-        Worker e1 = new Worker("Williams", 30, 2005, "Manager", "Male");
-        Worker e2 = new Worker("Collins", 31, 2006, "Cashier", "Male");
-        Worker e3 = new Worker("Andrew", 32, 2007, "Accountant", "Male");
-        Worker e4 = new Worker("Rover", 33, 2007, "Clerk", "Male");
+        Worker e1 = new Worker("Test1", 30, 2005, "Manager", "Male");
+        Worker e2 = new Worker("Test2", 31, 2006, "Cashier", "Male");
+        Worker e3 = new Worker("Test3", 32, 2007, "Accountant", "Male");
+        Worker e4 = new Worker("Test4", 33, 2007, "Clerk", "Male");
         return ResponseEntity.ok(Arrays.asList(e1, e2, e3, e4));
     }
 
